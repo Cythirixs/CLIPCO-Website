ccbd7b53727f9c6b09d630b360b8421e44677cfc
Jasper Edbrooke <jasperredrock@gmail.com>
2018-10-20 17:56:03 -0700

    remove clutter
    removed some old files from the repo

--- contact.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-	Welcome <?php echo $_POST["name"]; ?><br>
-	Your email address is: <?php echo $_POST["email"]; ?>
-	Comments: <?php echo $_POST["comments"]; ?>
-
-</body>
-</html>
